924a605e3e9bb33374fe089256466d7855b1bc6c
Sergey K <89184083452.mail.ru>
2020-11-23 00:13:36 +0300

    решение

--- src/test/java/ru/netology/AuthTest.java
+++ src/test/java/ru/netology/AuthTest.java
@@ -1,39 +1,59 @@
 package ru.netology;
 
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.filter.log.LogDetail;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.BeforeAll;
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static io.restassured.RestAssured.given;
 
 public class AuthTest {
-    private static RequestSpecification requestSpec = new RequestSpecBuilder()
-            .setBaseUri("http://localhost")
-            .setPort(9999)
-            .setAccept(ContentType.JSON)
-            .setContentType(ContentType.JSON)
-            .log(LogDetail.ALL)
-            .build();
 
-    @BeforeAll
-    static void setUpAll() {
-        // сам запрос
-        given() // "дано"
-                .spec(requestSpec) // указываем, какую спецификацию используем
-                .body(new RegistrationDto("vasya", "password", "active")) // передаём в теле
-                                                                  // объект, который будет преобразован в JSON
-                .when() // "когда"
-                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
-                .then() // "тогда ожидаем"
-                .statusCode(200); // код 200 OK
+    @BeforeEach
+    void befor() {
+        open("http://localhost:9999");
     }
-@Test
-    void gffg(){
 
-}
+    @Test
+    void inputValidLogPasStatActive() {
+        val user = DataGenerator.dataName("active");
+        DataGenerator.setUpAll(user);
+        $("span[data-test-id='login'] input").setValue(user.getLogin());
+        $("span[data-test-id='password'] input").setValue(user.getPassword());
+        $("button[data-test-id='action-login']").click();
+        $("body div#root h2").shouldHave(text("Личный кабинет"));
+    }
+
+    @Test
+    void inputValidLogPasStat_Bocked() {
+        val user = DataGenerator.dataName("blocked");
+        DataGenerator.setUpAll(user);
+        $("span[data-test-id='login'] input").setValue(user.getLogin());
+        $("span[data-test-id='password'] input").setValue(user.getPassword());
+        $("button[data-test-id='action-login']").click();
+        $("[class=notification__content]").shouldHave(text("Пользователь заблокирован"));
+    }
+
+    @Test
+    void inputNo_Valid_LogValidPasStatActive() {
+        val user = DataGenerator.dataName("active");
+        DataGenerator.setUpAll(user);
+        $("span[data-test-id='login'] input").setValue(DataGenerator.noValidLog());
+        $("span[data-test-id='password'] input").setValue(user.getPassword());
+        $("button[data-test-id='action-login']").click();
+        $("[class=notification__content]").shouldHave(text("Неверно указан логин или пароль"));
+    }
+    @Test
+    void inputValidLogNo_Valid_PasStatActive() {
+        val user = DataGenerator.dataName("active");
+        DataGenerator.setUpAll(user);
+        $("span[data-test-id='login'] input").setValue(user.getLogin());
+        $("span[data-test-id='password'] input").setValue(DataGenerator.noValidPass());
+        $("button[data-test-id='action-login']").click();
+        $("[class=notification__content]").shouldHave(text("Неверно указан логин или пароль"));
+    }
 //    ...
 //Для активации этого тестового режима при запуске SUT нужно указать флаг -P:profile=test, т.е.:
 // java -jar app-ibank.jar -P:profile=test
